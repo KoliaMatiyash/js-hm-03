f0f2eae98c5c7facd95e17bf2d02bc61c7c5d3b4
KoliaMatiyash <162808335+KoliaMatiyash@users.noreply.github.com>
2024-06-22 12:43:59 +0300

    last_add

--- task-1.js
+++ task-1.js
@@ -1,7 +1,7 @@
 const arr1 = [2200, 34000, 1200];
 const arr2 = [120000, 34000, 7000];
 const arr3 = [1100, 56000, 2700];
-const numbers = arr1.concat(arr2.concat(arr3));
+const numbers = arr1.concat(arr2, arr3);
 let min_number = numbers[0];
 for (const number of numbers) {
   if (min_number > number) {
